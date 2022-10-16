06369c6251fc3be44e7cfbae90836d9d3ff3eb13
wjwei-handsome <56633839+wjwei-handsome@users.noreply.github.com>
2022-10-17 00:26:57 +0800

    :bug: fix reference list
    fix a componet

--- src/components/ReferenceList/index.tsx
+++ src/components/ReferenceList/index.tsx
@@ -3,8 +3,6 @@ import styles from "./styles.module.css";
 import clsx from "clsx";
 import Link from "@docusaurus/Link";
 import ThemedImage from "@theme/ThemedImage";
-import LinkSVG from "@site/static/svg/link.svg";
-// import type LinkItemType from "@components/LinkItem";
 
 export type LinkItemType = {
     title: string;
@@ -44,7 +42,7 @@ export default function ReferenceList({
               <Link to={item.link} className={styles.titleArea}>
                 <div className={styles.title}>{item.title}</div>
                 <div className={styles.linkSVG}>
-                  <LinkSVG />
+                <svg xmlns="http://www.w3.org/2000/svg" width="12" height="16" viewBox="0 0 12 16"><path fill="#000" fill-rule="evenodd" d="M11 10h1v3c0 .55-.45 1-1 1H1c-.55 0-1-.45-1-1V3c0-.55.45-1 1-1h3v1H1v10h10v-3zM6 2l2.25 2.25L5 7.5 6.5 9l3.25-3.25L12 8V2H6z"/></svg>
                 </div>
               </Link>
             </li>
