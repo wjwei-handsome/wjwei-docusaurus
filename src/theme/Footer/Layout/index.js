70e63329604fb4a85bc32ad4bb303d843bd40c5f
wjwei-handsome <56633839+wjwei-handsome@users.noreply.github.com>
2023-12-22 00:59:21 +0800

    change footer

--- src/theme/Footer/Layout/index.js
+++ src/theme/Footer/Layout/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import clsx from 'clsx';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
-import {faGithubAlt, faDiscord} from '@fortawesome/free-brands-svg-icons';
+import { faGithubAlt, faWeixin } from '@fortawesome/free-brands-svg-icons';
+import { faCommentDots, faEnvelope } from '@fortawesome/free-solid-svg-icons';
 import styles from './style.module.css';
 
 
-export default function FooterLayout({style, links, logo, copyright}) {
+export default function FooterLayout({ style, links, logo, copyright }) {
   return (
     <footer
       className={clsx('footer', {
@@ -18,19 +18,21 @@ export default function FooterLayout({style, links, logo, copyright}) {
           <div className="footer__bottom text--center">
             {logo && <div className="margin-bottom--sm">{logo}</div>}
             <h4>{copyright}</h4>
-            <div id='contact-me' style={{"display":"flex","justifyContent":"center"}}>
+            <div id='contact-me' style={{ "display": "flex", "justifyContent": "center" }}>
               {/* <h4>Contact Me: */}
-                <a href="mailto:wjwei9908@gmail.com" className={styles.linkicon}>
-                  <FontAwesomeIcon icon={faEnvelope} style={{"fontSize": "36px"}}/>
-                </a>
-                <a href="https://github.com/wjwei-handsome" className={styles.linkicon}>
-                  <FontAwesomeIcon icon={faGithubAlt} style={{"fontSize": "36px"}}/>
-                </a>
-                <a href="https://discord.gg/dUyetKKSbv" className={styles.linkicon}>
-                  <FontAwesomeIcon icon={faDiscord} style={{"fontSize": "33px"}}/>
-                </a>
+              <a href="mailto:wjwei9908@gmail.com" className={styles.linkicon}>
+                <FontAwesomeIcon icon={faEnvelope} style={{ "fontSize": "36px" }} />
+              </a>
+              <a href="https://github.com/wjwei-handsome" className={styles.linkicon}>
+                <FontAwesomeIcon icon={faGithubAlt} style={{ "fontSize": "36px" }} />
+              </a>
+              <a href="/wec.html" className={styles.linkicon}>
+                <FontAwesomeIcon icon={faWeixin} style={{ "fontSize": "33px" }} />
+              </a>
+              <a href="https://matrix.to/#/@wjwei:matrix.org" className={styles.linkicon}>
+                <FontAwesomeIcon icon={faCommentDots} style={{ "fontSize": "33px" }} />
+              </a>
               {/* </h4> */}
-
             </div>
           </div>
         )}
